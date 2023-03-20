c4e463e994965c3f449ec5be1c78e0c93c7af84f
Veronika <versella@mail.ru>
2023-03-20 12:09:23 +0300

    InitialCommit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 10000;
+        int miles = 20;
+
+        int bonus = ticketPrice / miles;
+
+        System.out.println("Количество бонусных миль: " + bonus);
+    }
+}
